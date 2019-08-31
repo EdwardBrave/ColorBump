bd20adbb0c6118025deeab4f54543bd753bc829b
EdwardBrave <edward.brave45@gmail.com>
2019-08-31 22:06:20 +0300

    created main menu UI added MainScreen and Level1 scenes loaded source images and created basic skybox(by reference) programmed loading Level1 scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<string> levels;
+
+    public void LoadLevel(int levelIndex)
+    {
+        if (levelIndex < 0 || levelIndex >= levels.Count || levels[levelIndex] == "")
+            return;
+        Debug.Log(levels[levelIndex]);
+        SceneManager.LoadScene(levels[levelIndex]);
+    }
+}
---
+++ Assets/Scripts/UISwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISwitcher : MonoBehaviour
+{
+    public List<GameObject> panels;
+
+    public void Select(int panelIndex)
+    {
+        if (panelIndex < 0 || panelIndex >= panels.Count || !panels[panelIndex])
+            return;
+        foreach (GameObject panel in panels)
+            panel.SetActive(false);
+        panels[panelIndex].SetActive(true);
+    }
+}
