630ff81b48311c2091c58b6f5de8aa030f6e0e07
EdwardBrave <edward.brave45@gmail.com>
2019-08-31 22:08:54 +0300

    added bgMusic setting logic

---
+++ Assets/Scripts/MusicSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSwitcher : MonoBehaviour
+{
+    public Button bttnMusicOn;
+    public Button bttnMusicOff;
+
+    public static float bgMusicLevel = 1F;
+
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("BgMusicLevel"))
+            bgMusicLevel = PlayerPrefs.GetFloat("BgMusicLevel");
+        OnEnable();
+    }
+
+    void OnEnable()
+    {
+        if (bgMusicLevel > 0)
+            bttnMusicOn.Select();
+        else
+            bttnMusicOff.Select();
+    }
+
+    public void isMusicEnabled(bool val)
+    {
+        bgMusicLevel = val ? 1F : 0F;
+        PlayerPrefs.SetFloat("BgMusicLevel", bgMusicLevel);
+        OnEnable();
+        PlayerPrefs.Save();
+    }
+}
